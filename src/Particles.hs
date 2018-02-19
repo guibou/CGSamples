5664c719a3f1ef17b063f4df6952569a9e131b74
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2018-02-19 17:42:55 +0100

    A bit of particles cleaning

--- src/Particles.hs
+++ src/Particles.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE MultiWayIf #-}
-module Main where
+module Particles where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
@@ -11,8 +11,6 @@ import Data.Maybe (fromMaybe)
 import Control.Monad (guard, replicateM)
 import Data.Foldable (foldl')
 
-import Debug.Trace
-
 data V2 = V2 !Float !Float
   deriving (Show)
 
@@ -21,13 +19,14 @@ data Particle = Particle { position :: !V2
                          , velocity :: !V2
                          }
   deriving (Show)
+
 w,h :: Int
 w = 400
 h = 600
 
-run :: IO ()
-run = do
-  part <- makeScene nbParticules
+run :: Int -> IO ()
+run nbParticles = do
+  part <- makeScene nbParticles
 
   play (InWindow "Nice Window" (w, h) (10, 10)) white 24 part displayModel handleInput stepWorld
 
@@ -43,9 +42,9 @@ makeParticle x y = Particle p p (V2 0 0)
     p = V2 (0.2 + x * 0.6) (0.2 + y * 0.8)
 
 displayModel :: [Particle] -> Picture
-displayModel ps = translate (-fromIntegral (w `div` 2)) (-fromIntegral (h `div` 2)) $ Pictures (color black (Polygon [(0, 0), (fromIntegral w, 0), (fromIntegral w, fromIntegral h), (0, fromIntegral h)]) : map displayParticule ps)
+displayModel ps = translate (-fromIntegral (w `div` 2)) (-fromIntegral (h `div` 2)) $ Pictures (color black (Polygon [(0, 0), (fromIntegral w, 0), (fromIntegral w, fromIntegral h), (0, fromIntegral h)]) : map displayParticle ps)
 
-displayParticule (Particle (V2 x y) _ _) = translate (x * fromIntegral w) (y * fromIntegral h) (color red (circleSolid 2))
+displayParticle (Particle (V2 x y) _ _) = translate (x * fromIntegral w) (y * fromIntegral h) (color red (circleSolid 2))
 
 handleInput :: Event -> [t] -> [t]
 handleInput _e = id
@@ -55,7 +54,7 @@ stepWorld dt = (makeParticle 0.5 1 :)
                . map (nextVelocity dt)
                . (densityRelaxation dt radius)
                . map (applySpringDisplacement dt)
-               . map (advanceParticule dt)
+               . map (advanceParticle dt)
                . map (applyGravity dt)
 
 g :: V2
@@ -68,14 +67,11 @@ rho0 = 2
 radius = 0.05
 outsideForce = 500
 
-nbParticules :: Int
-nbParticules = 1000
-
 applyGravity :: Float -> Particle -> Particle
 applyGravity dt p = p { velocity = velocity p .+. dt *. g }
 
-advanceParticule :: Float -> Particle -> Particle
-advanceParticule dt p = p {oldPosition = position p,
+advanceParticle :: Float -> Particle -> Particle
+advanceParticle dt p = p {oldPosition = position p,
                            position = position p .+. dt *. velocity p
                           }
 
@@ -128,16 +124,16 @@ relax dt p qs = p {position = position p .-. deltaPos}
     getDisplacement q rij = (press * q + pressNear * sqr q) *. rij
 
 densityRelaxation :: Float -> Float -> [Particle] -> [Particle]
-densityRelaxation dt radius ps = zipWith (relax dt) ps (particulesDensity radius ps)
+densityRelaxation dt radius ps = zipWith (relax dt) ps (particlesDensity radius ps)
 
-particulesDensity :: Float -> [Particle] -> [[(Float, V2)]]
-particulesDensity radius ps = map (dFriends radius densityGrid) ps
+particlesDensity :: Float -> [Particle] -> [[(Float, V2)]]
+particlesDensity radius ps = map (dFriends radius densityGrid) ps
   where
     densityGrid = computeDensityGrid radius ps
 
 dFriends :: Float -> Grid -> Particle -> [(Float, V2)]
 dFriends radius grid p = do
-          let (x0, y0) = particuleHash radius p
+          let (x0, y0) = particleHash radius p
           x <- [x0 - 1..x0 + 1]
           y <- [y0 - 1..y0 + 1]
 
@@ -155,17 +151,9 @@ distance (V2 x y) (V2 x' y') = sqrt (sqr (x - x') + sqr (y - y'))
 sqr v = v * v
 
 computeDensityGrid :: Float -> [Particle] -> Grid
-computeDensityGrid radius ps = HashMap.fromListWith (\[x] xs -> x:xs) (map (\p -> (particuleHash radius p, [p])) ps)
+computeDensityGrid radius ps = HashMap.fromListWith (\[x] xs -> x:xs) (map (\p -> (particleHash radius p, [p])) ps)
 
-particuleHash :: Float -> Particle -> (Int, Int)
-particuleHash radius p = (truncate (x / radius), truncate (y / radius))
+particleHash :: Float -> Particle -> (Int, Int)
+particleHash radius p = (truncate (x / radius), truncate (y / radius))
   where
     V2 x y = position p
-
-{-
-doubleDensityRelaxation :: Float -> [Particle] -> [Particle]
-doubleDensityRelaxation dt particules
--}
-
-
-main = run
